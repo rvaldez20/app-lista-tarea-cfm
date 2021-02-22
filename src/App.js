065f121c366aab99726c315d096a7cacf8724259
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-22 14:17:06 -0600

    Se crea el componente Formulario y se le agregan estilos css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import React, {useState} from 'react';
 import './App.css';
 import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
 
 const App = () => {
+
+	
+
   	return (
 		<div className="contenedor">
 			<Header />
+
+			<FormularioTareas />
+
 		</div>
   );
 }
