1561ad2510132ee99aa9f5d5c3e0bdd50571b4ef
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-25 13:41:34 -0600

    Se igrega el listado el cual incluye 2 componentes (listado y tareas) con sus respectivos elementos(iconos y texto de cada tarea)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react';
 import './App.css';
+
 import Header from './componentes/Header';
 import FormularioTareas from './componentes/FormularioTareas';
+import ListaTareas from './componentes/ListaTareas';
 
 const App = () => {
 
 	// Se define el Estado
-	const [tareas, cambiarTareas] = useState([]);
+	const [tareas, cambiarTareas] = useState([
+		// {
+		// 	id: 1,
+		// 	texto: 'Lavar la ropa',
+		// 	completada: false
+		// },
+		// {
+		// 	id: 1,
+		// 	texto: 'Lavar la ropa',
+		// 	completada: false
+		// }
+	]);
 
 	console.log(tareas);
 
@@ -17,6 +30,10 @@ const App = () => {
 			<FormularioTareas 
 				tareas={tareas}
 				cambiarTareas={cambiarTareas}
+			/>
+
+			<ListaTareas 
+				tareas={tareas}
 			/>
 
 		</div>
