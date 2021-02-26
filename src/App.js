e0637f076c47e78213f51faf3889efdf4675e798
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-26 14:33:58 -0600

    Se agrega la funcionalidad de guardar en localStorage las tares y el estado para ver o no las completadas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import Header from './componentes/Header';
@@ -7,25 +7,48 @@ import ListaTareas from './componentes/ListaTareas';
 
 const App = () => {
 
-	// Se define el Estado para as tareas
-	const [tareas, cambiarTareas] = useState([
-		{
-			id: 1,
-			texto: 'Lavar la ropa',
-			completada: false
-		},
-		{
-			id: 2,
-			texto: 'Bañar al perro',
-			completada: true
-		}
-	]);
+	//MI LOGICA -obtenemos los datos que tenemos guardados en localstorage, si no hay datos es un array vacio 
+	// let tareasGuardadas = localStorage.getItem('tareas');
+	// if (tareasGuardadas === null) {
+	// 	tareasGuardadas = [];
+	// } else {
+	// 	tareasGuardadas = JSON.parse(localStorage.getItem('tareas'));
+	// }
 
-	// se define el state para mostrar | ocultar las tareas Completadas
-	const [mostrarCompletadas, cambiarmostrarCompletadas] = useState(true);
+	/* ++++++++++++++++ useEstat y useEffect para as tareas ++++++++++++++++ */
 
+	//obtenemos los datos que tenemos guardados en localstorage, si no hay sera [] 
+	const tareasGuardadas = localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : [];	
+
+	// Se define el Estado para as tareas   localStorage.getItem('tareas') 
+	const [tareas, cambiarTareas] = useState(tareasGuardadas);
 	// console.log(tareas);
 
+	// definimos el useEffect para guardar el estado de las tareas en localstorage
+	useEffect(() => {	
+		localStorage.setItem('tareas', JSON.stringify(tareas));			
+	},[tareas]);
+
+
+	/* +++++++++++++ useEstat y useEffect para ver o no completadas +++++++++++++ */
+
+	// Accedemos al localstorage y comprobar si mostrarCompletadas es null
+	let configMostrarCompletadas = '';
+	if (localStorage.getItem('mostrarCompletadas') === null) {
+		configMostrarCompletadas = true;
+	} else {		
+		configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+	}
+
+	// se define el state para mostrar | ocultar las tareas Completadas
+	const [mostrarCompletadas, cambiarmostrarCompletadas] = useState(configMostrarCompletadas);
+
+	// definimos el useEffect para guardar el estado del boton ocultar y mostrar completadas
+	useEffect(() => {	
+		localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+	},[mostrarCompletadas]);
+
+	
   	return (
 		<div className="contenedor">
 			<Header 
